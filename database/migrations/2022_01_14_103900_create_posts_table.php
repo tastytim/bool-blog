f582ad7606fe7eafbdf7f1466a9e646e94f50371
yanitejeda <tejedasp@gmail.com>
2022-01-14 11:46:45 +0100

    aggiunto migration post

---
+++ database/migrations/2022_01_14_103900_create_posts_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('subtitle')->nullable();
+            $table->longtext('content');
+            $table->string('author');
+            $table->string('coverImg')->nullable();
+            $table->string('category');
+            $table->timestamps();
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posts');
+    }
+}
