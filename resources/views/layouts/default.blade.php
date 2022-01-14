ae27cfa441a60c051883dfb4e0fb083fb089d982
tastytim <tastytimgm@gmail.com>
2022-01-14 12:35:50 +0100

    created layout

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>@yield('title')</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="{{asset("assets/favicon.ico")}}" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="{{asset("css/styles.css")}}" rel="stylesheet" />
+    </head>
+    <body>
+        
+        @include("partials.navbar")
+       
+        @yield('page_header')
+        
+        <div class="container">
+            <div class="row">
+                
+                <div class="col-lg-8">
+                   
+                    @yield("main_content")
+                </div>
+               
+                <div class="col-lg-4">
+                   @include("partials.sidebar")
+                </div>
+            </div>
+        </div>
+        <!-- Footer-->
+        @include('partials.footer')
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="{{asset("js/scripts.js")}}"></script>
+    </body>
+</html>
