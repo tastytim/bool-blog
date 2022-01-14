cc2a1aa06d73198289e70bf78507f6614a8a802a
tastytim <tastytimgm@gmail.com>
2022-01-14 18:00:39 +0100

    added posts route to nav

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -4,9 +4,9 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+            <li class="nav-item"><a class="nav-link link-light" href="{{route('home.index')}}">Home</a></li>
+            <li class="nav-item"><a class="nav-link link-light" href="{{route('posts.index')}}">Posts</a></li>
                 <li class="nav-item"><a class="nav-link link-light" href="{{route("posts.create")}}">Create</a></li>
-               {{--  <li class="nav-item"><a class="nav-link link-light" href="{{route("")}}">Add</a></li>
-               --}}  <li class="nav-item"><a class="nav-link link-light" href="{{route('home.index')}}">Home</a></li>
             </ul>
         </div>
     </div>
