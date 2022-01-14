df45b8e198073686ba707670e1f438da22417b37
tastytim <tastytimgm@gmail.com>
2022-01-14 17:52:37 +0100

    fixed index

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,24 +5,25 @@
 <div class="container">
     <h1>Lista ricette</h1>
     <br>
-    <button><a href="{{route("posts.create")}}">Aggiungi nuova ricetta</button>
-    <ul class="cards post_ricetta">
+    <div><button class="btn btn-dark">
+<a href={{route("posts.create")}}>Create Post</a></button></div>
+    <ul class="">
         @foreach($posts as $post)
         <li><a href="{{ route('posts.show', $post->id) }}">
                 <div>
                     <img src="{{$post->coverImg}}">
                 </div>
                 <div>
-                <h2>
-                 {{$post->title}}
-                </h2>
-                   
+                    <h2>
+                        {{$post->title}}
+                    </h2>
+
                 </div>
                 <div>
-                <h3>
-                {{$post->subtitle}}
-                </h3>
-                    
+                    <h3>
+                        {{$post->subtitle}}
+                    </h3>
+
                 </div>
             </a></li>
         @endforeach
